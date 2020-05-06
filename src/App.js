139f2f5c95aa35595ddcd08e66942f9d8c7a9de1
ashutosh diwakar <ashutosh.diwakar@mountblue.tech>
2020-05-06 12:18:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-// import NavbarInstance from './Navbar'
-// import Footer from './footer'
+
 import './App.css';
-import Home from './homepage'
+
 
 function App() {
   return (
     <div className="App">
      
-   <Home/>
    
     </div>
   );
