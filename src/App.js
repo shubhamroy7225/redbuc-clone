eb2b0439d15f3b48568774e9d2a21aa2cf7c6c20
shubham jayswal <shubhamjayswal10302@gmail.com>
2020-05-09 15:05:28 +0530

    Created signin and signup forms
    Added the validation for signup form,
    Created loginhomepage for signin and signup forms.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Home from "./homepage";
-
 function App() {
   return (
     <div className="App">
--- src/homepage.js
+++ src/homepage.js
@@ -2,13 +2,15 @@ import React from "react";
 import NavbarInstance from "./Navbar";
 import Footer from "./footer";
 import Content from "./Content";
+import LoginHomePage from "./LoginHomePage";
 
 function Home() {
   return (
     <div className="d-flex flex-column sticky-footer-wrapper">
-      <NavbarInstance />
+      <LoginHomePage />
+      {/* <NavbarInstance />
       <Content className="flex-fill" />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
